402669aa6605816321a135f6dbd1de0630052c68
Soundarya Sundari <49031382+codergit1@users.noreply.github.com>
2022-11-30 12:22:39 +0000

    Parsed and Created .csv file to store

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
+import pandas as pd
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+
 from selenium.webdriver.common.keys import Keys
 
 # When Chromedriver crashed while importing Selenium, the solution's given below
@@ -11,6 +14,9 @@ def get_driver():
 
   #Initializing options for chromedriver
   chrome_options = Options()
+
+  chrome_options.page_load_strategy = 'normal'
+
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--headless')
   chrome_options.add_argument('--disable-dev-shm-usage')
@@ -20,32 +26,29 @@ def get_driver():
   return gchrome_driver
 
 
-
 def get_videos(driver):
   # YT_TRENDING_URL = "https://www.youtube.com/feed/trending"
 
-  YT_TRENDING_URL = "https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl"
-
   VIDEO_DIV_TAG = 'ytd-video-renderer'
 
+  YT_TRENDING_URL = "https://www.youtube.com/feed/trending?bp=6gQJRkVleHBsb3Jl"
+
   driver.get(YT_TRENDING_URL)
-  #driver.implicitly_wait(100)
-  
+  driver.implicitly_wait(100000)
+
   print('Getting video div tags')
   vid_tag = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
 
   return vid_tag
 
 
-
 def parse_video(video):
-  vid = video
-  title_tag = vid.find_element(By.ID, 'video-title')
+  title_tag = video.find_element(By.ID, 'video-title')
 
   # print(f'Title: {(title_tag)}')
 
   # Found title by searching with 'video-title', but since it's an object therefore use .text to show title in     text
-  
+
   title = title_tag.text
 
   #EXTRA NOTE: Use 'find_element' not 'find_elements'
@@ -56,19 +59,19 @@ def parse_video(video):
   print('URL: ', url)
 
   #thumbnail & thumbnail_url
-  thumbnail_tag = vid.find_element(By.TAG_NAME, 'img')
+  thumbnail_tag = video.find_element(By.TAG_NAME, 'img')
 
   thumbnail_url = thumbnail_tag.get_attribute('src')
 
   print('Thumbnail URL: ', thumbnail_url)
 
   #Channel
-  channel = vid.find_element(By.CLASS_NAME, 'ytd-channel-name')
+  channel = video.find_element(By.CLASS_NAME, 'ytd-channel-name')
   channel_name = channel.text
   print('Channel: ', channel_name)
 
   #Description
-  description = vid.find_element(By.ID, 'description-text').text
+  description = video.find_element(By.ID, 'description-text').text
   print('Description: ', description)
 
   return {
@@ -88,9 +91,9 @@ if __name__ == '__main__':
   print("Fetching Page")
   video = get_videos(driver)
 
-  print('Page Title: ', driver.title) 
+  print('Page Title: ', driver.title)
   # driver.quit() when used it found 1 video div tag
-  
+
   print(f'Found {len(video)} video div tags')  #can be printed
 
   #Title, url, thumbnail_url, channel, views, uploaded, description
@@ -98,4 +101,8 @@ if __name__ == '__main__':
   vid_data = [parse_video(v) for v in video[:]]
   print(vid_data)
 
-  
+  #PLEASE NOTE: Webdriver loaded fast and exited
+  print('Save data to CSV')
+  videos_df = pd.DataFrame(vid_data)
+  print(videos_df)
+  videos_df.to_csv('trending.csv') 
